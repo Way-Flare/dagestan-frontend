a62b5d02579163c2b6d5fc4b11d185a2e7e2bdd7
albibe <94756449+albibe@users.noreply.github.com>
2023-10-16 17:36:56 +0300

    DAGESTAN-27 Добавить в проект husky

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,14 +1,14 @@
-import { configureStore, combineReducer } from "@reduxjs/toolkit"
-import { apiSlice } from "../../shared/api/defaultApi"
+import { configureStore, combineReducer } from "@reduxjs/toolkit";
+import { apiSlice } from "../../shared/api/defaultApi";
 
 const rootReducer = combineReducer({
   [apiSlice.reducerPath]: apiSlice.reducer,
-})
+});
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
-})
+});
 
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
