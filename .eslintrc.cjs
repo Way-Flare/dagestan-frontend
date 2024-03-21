a1eddf9f827ec68fb2ebde844d06e9fa91ae673f
Vladimir Khlebnikov <khlebnikovvladimir19@gmail.com>
2024-03-21 16:57:19 +0300

    husky, prettier
    added stylelint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,9 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": [
+  extends: [
     "eslint:recommended",
     "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/recommended",
@@ -12,40 +12,36 @@ module.exports = {
     "plugin:import/warnings",
     "plugin:import/typescript",
     "plugin:prettier/recommended",
-    "prettier"
+    "prettier",
   ],
-  "overrides": [
+  overrides: [
     {
-      "env": {
-        "node": true
+      env: {
+        node: true,
       },
-      "files": [
-        ".eslintrc.{js,cjs}"
-      ],
-      "parserOptions": {
-        "sourceType": "script"
-      }
-    }
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  "plugins": [
-    "@typescript-eslint",
-    "react",
-    "prettier"
-  ],
-  "rules": {
+  plugins: ["@typescript-eslint", "react", "prettier"],
+  rules: {
+    "comma-dangle": "off",
+    "@typescript-eslint/comma-dangle": "off",
     "react/prop-types": "off",
     "typescript-eslint/ban-ts-commit": "off",
     "typescript-eslint/ban-types": "off",
     "react/react-in-jsx-scope": "off",
     "import/no-unresolved": "off",
     "no-unused-vars": "off",
-    "semi": [2, "never"],
+    semi: [2, "never"],
     "react/display-name": "off",
-    "@typescript-eslint/no-unused-vars": "off"
-  }
+    "@typescript-eslint/no-unused-vars": "off",
+  },
 }
