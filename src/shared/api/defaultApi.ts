879f43d878cfbe8837cc337cd1d3bbe551874a23
Vladimir Khlebnikov <khlebnikovvladimir19@gmail.com>
2024-04-17 17:57:37 +0300

    DAGESTAN-119: added request to prod, proxy on dev

--- src/shared/api/defaultApi.ts
+++ src/shared/api/defaultApi.ts
@@ -4,12 +4,11 @@ import { rtk_tags } from "./tags"
 export const apiSlice = createApi({
   reducerPath: "api",
   tagTypes: Object.values(rtk_tags),
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_HOST }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.API_HOST }),
   endpoints: (builder) => ({
     getPosts: builder.query({
       query: () => "/posts",
     }),
   }),
 })
-
 export const { useGetPostsQuery } = apiSlice
